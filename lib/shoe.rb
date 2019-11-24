c46aa60fce30b8f97b3db2675ba90306306b81c0
Melissa Holt <melissa.holt21@gmail.com>
2019-11-24 23:02:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,7 +10,13 @@ class Shoe
       BRANDS << @brand
     end
   end
-
+  
+  def initialize(brand)
+    @brand = brand
+    BRANDS << @brand
+  end
+  
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
