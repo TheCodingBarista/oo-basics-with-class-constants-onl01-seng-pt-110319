d3796e6ba165e396b94574e35546978308e249cb
Melissa Holt <melissa.holt21@gmail.com>
2019-11-24 22:39:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  Brands = []
+  brands = []
 
   def initialize(brand)
     @brand = brand
-    Brands << brand
+    brands << brand
 Brands.uniq
   end
 
